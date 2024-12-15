255f82d862026af2569e2d0385e6a591c4f674d2
Nicolas DUBIEN <github@dubien.org>
2024-12-15 11:29:41 +0000

    pretty print the answer

--- test/day-11.spec.js
+++ test/day-11.spec.js
@@ -18,27 +18,36 @@ const inputsArbitrary = marketDimensionsArbitrary.chain(({ width, height }) =>
       width: fc.integer({ min: 1, max: width }),
       height: fc.integer({ min: 1, max: height }),
     }),
+    ctx: fc.context(),
   })
 );
 
 test.prop([inputsArbitrary])(
   "day #11: should find a place for Santa as there is one by construct (top left)",
-  ({ rawMap, requestedArea }) => {
+  ({ rawMap, requestedArea, ctx }) => {
     const map = structuredClone(rawMap);
     for (let j = 0; j !== requestedArea.height; ++j) {
       for (let i = 0; i !== requestedArea.width; ++i) {
         map[j][i] = true;
       }
     }
+    ctx.log(
+      map.map((row) => row.map((c) => (c ? "x" : ".")).join("")).join("\n")
+    );
+
     expect(findPlaceForSanta(map, requestedArea)).toBeDefined();
   }
 );
 
 test.prop([inputsArbitrary])(
   "day #11: should find a place where Santa fits (when finding one)",
-  ({ rawMap, requestedArea }) => {
+  ({ rawMap, requestedArea, ctx }) => {
     const out = findPlaceForSanta(rawMap, requestedArea);
     fc.pre(out !== undefined);
+    ctx.log(
+      rawMap.map((row) => row.map((c) => (c ? "x" : ".")).join("")).join("\n")
+    );
+
     for (let j = 0; j !== requestedArea.height; ++j) {
       for (let i = 0; i !== requestedArea.width; ++i) {
         expect(rawMap[out.y + j][out.x + i]).toBe(true);
