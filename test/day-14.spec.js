01b348352b9ffa97c0a2466fd0b5959bd605e9b6
Nicolas DUBIEN <github@dubien.org>
2024-12-20 16:40:07 +0000

    Another property for day 14

--- test/day-14.spec.js
+++ test/day-14.spec.js
@@ -10,3 +10,19 @@ test.prop([fc.string({ unit: "binary" })])(
     expect(compressor.decompress(compressor.compress(text))).toBe(text);
   }
 );
+
+test.prop([
+  fc.array(
+    fc.record({
+      count: fc.integer({ min: 1, max: 10000 }),
+      char: fc.string({ unit: "grapheme-ascii", minLength: 1, maxLength: 1 }),
+    })
+  ),
+])(
+  "day #14: could revert itself with strings having lots of repeats",
+  (textDetails) => {
+    const compressor = buildCompressor();
+    const text = textDetails.map((e) => e.char.repeat(e.count)).join("");
+    expect(compressor.decompress(compressor.compress(text))).toBe(text);
+  }
+);
